ec7e8b97cee49b8f913f377b38d20090a7c76b2e
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-06-06 17:36:57 +0200

    Start Project and creating login form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 function LoginForm() {
     return (
         <div className="bg-[url('src/assets/spaceBg.jpg')] h-screen flex justify-center">
-            <p className="text-gray-200  bg-slate-200 w-1/3 h-1/2 pt-4 flex justify-center text-black">Create Your Space</p>
+            <p className="text-violet-500  bg-slate-200 w-1/3 h-1/2 pt-4 flex justify-center">Create Your Space</p>
         </div>
         
     )
