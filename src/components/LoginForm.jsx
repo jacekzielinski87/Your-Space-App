49dedb5a908deb4178f0ad25349e56e304fda3e5
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-06-06 20:42:41 +0200

    Login Form

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,7 +1,7 @@
 function LoginForm() {
     return (
         <div className="bg-[url('src/assets/spaceBg.jpg')] h-screen flex justify-center">
-            <p className="text-violet-500  bg-slate-200 w-1/3 h-1/2 pt-4 flex justify-center">Create Your Space</p>
+            <p className=" text-violet-800 bg-slate-100 w-1/3 h-1/3 pt-4 flex justify-center text-4xl mt-24 rounded-xl font-extra">Create Your Space</p>
         </div>
         
     )
