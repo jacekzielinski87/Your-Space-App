41fa051921afc9b8e2830cda680faeca9f1d1f78
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-06-08 13:06:41 +0200

    Sidebar menu setup

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -1,18 +1,24 @@
+
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faHouse,  } from '@fortawesome/free-solid-svg-icons';
-
-
-
+import {faHouse, faPerson, faNewspaper, faUpload} from '@fortawesome/free-solid-svg-icons';
 
 
 function SidebarMenu() {
     return (
-        <div>
-            <h2>Discover</h2>
+        <>
+        <div className='bg-slate-950 text-slate-100 p-4 w-40 h-50'>
             <FontAwesomeIcon icon={faHouse}/>
-            <p>Home</p>
-            <p>People</p>
+            <p className='flex items-center justify-center'>Home</p>
+            <FontAwesomeIcon icon={faPerson} />
+            <p className='flex items-center justify-center'>People</p>
+            <FontAwesomeIcon icon={faNewspaper} />
+            <p className='flex items-center justify-center'>Articles</p>
+            </div>
+        <div className='absolute right-10 top-0 bg-slate-200 text-slate-900 text-xl font-medium'>
+        <button className='p-4'>Upload</button>
+        <FontAwesomeIcon icon={faUpload} className='pr-6'/>
         </div>
+        </>
     )
 }
 
