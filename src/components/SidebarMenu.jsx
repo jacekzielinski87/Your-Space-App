6c4b023c3edb657ce98e4d1d13e22e0c0d4b4025
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-06-08 15:05:59 +0200

    Sidebar menu setup

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -7,12 +7,18 @@ function SidebarMenu() {
     return (
         <>
         <div className='bg-slate-950 text-slate-100 p-4 w-40 h-50'>
+            <div className='flex p-2 justify-center items-center gap-3'>
             <FontAwesomeIcon icon={faHouse}/>
-            <p className='flex items-center justify-center'>Home</p>
+            <p>Home</p>
+            </div>
+            <div className='flex p-2 justify-center items-center gap-3'>
             <FontAwesomeIcon icon={faPerson} />
             <p className='flex items-center justify-center'>People</p>
+            </div>
+            <div className='flex p-2 justify-center items-center gap-3'>
             <FontAwesomeIcon icon={faNewspaper} />
             <p className='flex items-center justify-center'>Articles</p>
+            </div>
             </div>
         <div className='absolute right-10 top-0 bg-slate-200 text-slate-900 text-xl font-medium'>
         <button className='p-4'>Upload</button>
