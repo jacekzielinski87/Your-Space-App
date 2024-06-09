9029398667f02021f287bd67fa1df3013fd4b3ff
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-06-09 16:38:33 +0200

    Homepage setup

--- src/components/SidebarMenu.jsx
+++ src/components/SidebarMenu.jsx
@@ -7,28 +7,28 @@ function SidebarMenu() {
 
     return (
         <>
-        <div>
-            <FontAwesomeIcon icon={faBars} className='py-6 px-4 text-3xl text-slate-100 cursor-pointer'/>
+        <div className='flex justify-end'>
+            <FontAwesomeIcon icon={faBars} className=' text-slate-200 cursor-pointer absolute left-10 top-5 h-10'/>
         </div>
-        < div className='bg-slate-950 text-slate-100 py-24 w-32 h-60 absolute top-0 hidden'>
-            <div className='flex p-3 justify-center items-center gap-3 relative bottom-20'>
+        <ul className='bg-slate-950 text-slate-100 py-24 w-32 h-60 absolute top-0 hidden'>
+            <li className='flex p-3 justify-center items-center gap-3 relative bottom-20'>
             <FontAwesomeIcon icon={faUser}/>
             <a href='#' className='flex items-center justify-center'>Profile</a>
-            </div>
-            <div className='flex p-3 justify-center items-center gap-3 relative bottom-20'>
+            </li>
+            <li className='flex p-3 justify-center items-center gap-3 relative bottom-20'>
             <FontAwesomeIcon icon={faPerson} />
             <a href='#' className='flex items-center justify-center'>People</a>
-            </div>
-            <div className='flex p-3 justify-center items-center gap-3 relative bottom-20'>
+            </li>
+            <li className='flex p-3 justify-center items-center gap-3 relative bottom-20'>
             <FontAwesomeIcon icon={faNewspaper} />
             <a href='#' className='flex items-center justify-center'>Articles</a>
-            </div>
-            <div className='flex p-3 justify-center items-center gap-3 relative bottom-20'>
+            </li>
+            <li className='flex p-3 justify-center items-center gap-3 relative bottom-20'>
             <FontAwesomeIcon icon={faHouse} />
             <a href='#' className='flex items-center justify-center'>Home</a>
             <div/>
-            </div>
-        </div>
+            </li>
+        </ul>
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,21 @@
 import SidebarMenu from "../components/SidebarMenu"
-import Space from '../assets/yourSpaceBoxx.jpg';
+import Space from '../assets/yourSpaceBox.jpg';
 
 
 function Home() {
     return (
         <>
-        <div className="bg-slate-900 h-full">
+        <div>
         <div>
             <SidebarMenu/>
         </div>
-        <div className="flex justify-center">
-            <div className="bg-slate-100">
-                <img className="m-2 rounded-xl w-2/4" src={Space}></img>
-                <p className="font-Dosis font-medium text-2xl text-slate-700">Your space is place where<br/> you can show your work to the whole world ! </p>
-            </div>
+        <div className="bg-stone-950 h-screen">
+            <div className="flex justify-center items-center p-16">
+            <p className="font-Dosis font-bold text-5xl text-slate-100 absolute p-12">Your space is place where<br/> you can show your work to the whole world ! </p>
         </div>
-        <div className="flex justify-center">
-            <div className="">
-                <img className="m-2 rounded-xl w-2/4" src={Space}></img>
-                <p className="font-Dosis font-medium text-2xl text-slate-100">Your space is place where<br/> you can show your work to the whole world ! </p>
-            </div>
+        <div className="flex justify-center py-4 opacity-80">
+            <img className="m-2 rounded-xl w-2/4" src={Space}></img>
         </div>
-        <div className="flex justify-center">
-            <div className="">
-                <img className="m-2 rounded-xl w-2/4" src={Space}></img>
-                <p className="font-Dosis font-medium text-2xl text-slate-100">Your space is place where<br/> you can show your work to the whole world ! </p>
-            </div>
         </div>
         </div>
         </>
