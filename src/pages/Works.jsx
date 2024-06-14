067aac5eee75b862b7186aa95da27b06355b752b
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-06-14 21:40:09 +0200

    Works flexbox items

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -5,15 +5,15 @@ import Ideas from '../assets/Lights.jpg';
 
 function Works() {
     return (
-        <div className="bg-zinc-300">
+        <div className="bg-zinc-300/90">
             <Menu/>
-            <div className="flex items-start justify-start">
-                <h1 className="font-Rubik">This is space for your works !</h1>
-                <img className=" w-1/3 m-4 rounded-xl" src={Workspace} alt="workspace image"></img>
-            </div>
-            <div className="bg-zinc-400 flex justify-end">
-                <h1 className="font-Rubik">This is space for your works !</h1>
+            <div>
+                <h1 className="font-Rubik text-2xl p-6 ">Share your ideas with others.</h1>
                 <img className="w-1/3 m-4 rounded-xl" src={Ideas} alt="workspace image"></img>
+            </div>
+            <div className="bg-zinc-400/65">
+                <h1 className="font-Rubik text-2xl p-6">This is space for your works.</h1>
+                <img className=" w-1/3 m-4 rounded-xl" src={Workspace} alt="workspace image"></img>
             </div>
         </div>
     )
