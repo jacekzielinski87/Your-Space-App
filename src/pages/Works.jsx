b80b746195425d7a0517e0f23042aa664a7c685f
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-06-15 10:03:28 +0200

    New features

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -1,19 +1,22 @@
 import Menu from "../components/Menu"
 import Workspace from '../assets/Workspace.jpg';
 import Ideas from '../assets/Lights.jpg';
+import Turn from '../assets/Turn.jpg';
 
 
 function Works() {
     return (
-        <div className="bg-zinc-300/90">
+        <div>
             <Menu/>
-            <div>
-                <h1 className="font-Rubik text-2xl p-6 ">Share your ideas with others.</h1>
-                <img className="w-1/3 m-4 rounded-xl" src={Ideas} alt="workspace image"></img>
+            <div className="bg-slate-300/70 flex justify-start items-center mx-12">
+                <h1 className="font-Rubik text-2xl flex">Share your <br/>ideas <br/>with others</h1>
+                <img className="w-1/3 m-4 rounded-xl mx-12" src={Ideas} alt="workspace image"></img>
+                <h1 className="font-Rubik text-2xl">Share your<br/> ideas</h1>
+                <img className=" w-1/3 m-4 rounded-xl"  src={Turn} alt="workspace image"></img>
             </div>
-            <div className="bg-zinc-400/65">
-                <h1 className="font-Rubik text-2xl p-6">This is space for your works.</h1>
-                <img className=" w-1/3 m-4 rounded-xl" src={Workspace} alt="workspace image"></img>
+            <div className="flex justify-center bg-slate-300/70 m-12">
+                <h1 className="font-Rubik text-2xl flex items-center">This is space for<br/> your works.</h1>
+                <img className=" w-1/3 m-4 rounded-xl shadow-xl" src={Workspace} alt="workspace image"></img>
             </div>
         </div>
     )
