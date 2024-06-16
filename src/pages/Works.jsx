82007c01369bb3ef5ddab1e39cf9a0648de267ea
Jacek Zielinski <jacekzielinski0787@gmail.com>
2024-06-16 11:05:11 +0200

    New Works Pattern

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -6,17 +6,14 @@ import Turn from '../assets/Turn.jpg';
 
 function Works() {
     return (
-        <div>
+        <div className="m-8">
             <Menu/>
-            <div className="bg-slate-300/70 flex justify-start items-center mx-12">
-                <h1 className="font-Rubik text-2xl flex">Share your <br/>ideas <br/>with others</h1>
+            <div className="bg-slate-300/70 flex justify-center items-center">
                 <img className="w-1/3 m-4 rounded-xl mx-12" src={Ideas} alt="workspace image"></img>
-                <h1 className="font-Rubik text-2xl">Share your<br/> ideas</h1>
-                <img className=" w-1/3 m-4 rounded-xl"  src={Turn} alt="workspace image"></img>
-            </div>
-            <div className="flex justify-center bg-slate-300/70 m-12">
-                <h1 className="font-Rubik text-2xl flex items-center">This is space for<br/> your works.</h1>
                 <img className=" w-1/3 m-4 rounded-xl shadow-xl" src={Workspace} alt="workspace image"></img>
+            </div>
+            <div className="flex justify-center bg-slate-300/70">
+                <img className=" w-1/3 m-4 rounded-xl"  src={Turn} alt="workspace image"></img>
             </div>
         </div>
     )
